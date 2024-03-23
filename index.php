08a599b0eb82ec4b713250564cf6efa300b2e728
galihsuks <galih8.4.2001@gmail.com>
2024-03-24 06:44:14 +0700

    first

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$produk = [
+    'status' => 'ok',
+    'data' => [
+        [
+            'nama' => 'Meja',
+            'harga' => 200000,
+            'varian' => ['kayu', 'besi', 'putih']
+        ],
+        [
+            'nama' => 'Kursi',
+            'harga' => 265000,
+            'varian' => ['bundar', 'kotak']
+        ],
+        [
+            'nama' => 'Lemari',
+            'harga' => 1500000,
+            'varian' => ['kayu', 'putih', 'hitam']
+        ],
+        [
+            'nama' => 'Rak',
+            'harga' => 170000,
+            'varian' => ['3 Susun', '4 Susun']
+        ],
+    ]
+];
