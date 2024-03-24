18bcb1f5bcf335653591c81fe54e143190c7a75b
galihsuks <galih8.4.2001@gmail.com>
2024-03-24 07:11:28 +0700

    udpate comment

--- index.php
+++ index.php
@@ -4,23 +4,29 @@ $produk = [
     'data' => [
         [
             'nama' => 'Meja',
-            'harga' => 200000,
-            'varian' => ['kayu', 'besi', 'putih']
+            'harga' => 200000, //dalam rupiah
+            'varian' => ['kayu', 'besi', 'putih'],
+            'diskon' => 10, //dalam persen
         ],
         [
             'nama' => 'Kursi',
-            'harga' => 265000,
-            'varian' => ['bundar', 'kotak']
+            'harga' => 265000, //dalam rupiah
+            'varian' => ['bundar', 'kotak'],
+            'diskon' => 20, //dalam persen
         ],
         [
             'nama' => 'Lemari',
             'harga' => 1500000,
-            'varian' => ['kayu', 'putih', 'hitam']
+            'varian' => ['kayu', 'putih', 'hitam'],
+            'diskon' => 30, //dalam persen
         ],
         [
             'nama' => 'Rak',
             'harga' => 170000,
-            'varian' => ['3 Susun', '4 Susun']
+            'varian' => ['3 Susun', '4 Susun'],
+            'diskon' => 40, //dalam persen
         ],
     ]
 ];
+
+//tulis kodemu dibawah sini
